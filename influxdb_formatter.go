80d262233d64e31a08221b86681815ccb98cf39a
Jun Luo <junl@ubuntu.none>
2016-11-10 19:51:53 +0800

    update tag

--- influxdb_formatter.go
+++ influxdb_formatter.go
@@ -5,6 +5,7 @@ import (
     "bytes"
     "fmt"
     "time"
+    "strings"
 )
 
 //default measurement name and host name.
@@ -55,16 +56,18 @@ func (f *InfluxdbFormat) Format(entry *Entry) ([]byte, error) {
 	}
 
 	prefixFieldClashes(entry.Data)
+    msg := strings.Replace(strings.Replace(entry.Message, "\n", " ", -1), `"`, `\"`, -1)
     //sl.logger.Printf(`user=%s,db=%s usetime=%d,node="%s",ip="%s",sql="%s" %d`, user, db, t, node, ip, sql, time.Now().UnixNano())
     b.WriteString(fmt.Sprintf(`%s,host=%s,`, f.Measurement, f.Host))
-	b.WriteString(fmt.Sprintf(`user=%s,db=%s usetime=%d,node="%s",ip="%s",sql="%s" %d\n`,
-        entry.Data["user"],
+	b.WriteString(fmt.Sprintf(`user=%s,db=%s,node=%s,ip=%s usetime=%d,sql="%s" %d`,
+         entry.Data["user"],
          entry.Data["db"],
-         entry.Data["usetime"],
          entry.Data["node"],
          entry.Data["ip"],
-         entry.Data["sql"],
+         entry.Data["usetime"],
+         msg,
          time.Now().UnixNano(),
     ))
+    b.WriteByte('\n')
 	return b.Bytes(), nil
 }
