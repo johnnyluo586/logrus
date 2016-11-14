f06f3ec97479aaac94ef76b78f91ad1c5e351442
Jun Luo <junl@ubuntu.none>
2016-11-14 15:14:34 +0800

    add caller

---
+++ hooks/caller/caller.go
@@ -0,0 +1,46 @@
+package caller
+
+import (
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
+
+	"github.com/Sirupsen/logrus"
+)
+
+// CallerHook 需要的钩子
+type CallerHook struct {
+}
+
+// Fire 实现
+func (hook *CallerHook) Fire(entry *logrus.Entry) error {
+	if len(entry.Data) == 0 { // if don't use withfields
+		entry.Data["caller"] = hook.caller(7)
+	} else { // if use withfields
+		entry.Data["caller"] = hook.caller(5)
+	}
+
+	return nil
+}
+
+// Levels 实现的级别
+func (hook *CallerHook) Levels() []logrus.Level {
+	return []logrus.Level{
+		logrus.PanicLevel,
+		logrus.FatalLevel,
+		logrus.ErrorLevel,
+		logrus.WarnLevel,
+		logrus.InfoLevel,
+		logrus.DebugLevel,
+	}
+}
+
+// caller 带withfields的函数调用
+func (hook *CallerHook) caller(skip int) string {
+	if _, file, line, ok := runtime.Caller(skip); ok {
+		return strings.Join([]string{filepath.Base(file), strconv.Itoa(line)}, ":")
+	}
+	// not sure what the convention should be here
+	return "???"
+}
